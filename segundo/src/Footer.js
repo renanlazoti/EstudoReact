db8086bb86a7b0f95c09da056f07ab7c9fdaafe6
RENAN FERREIRA LAZOTI <renan.flazoti@senacsp.edu.br>
2022-05-11 16:56:09 -0300

    Segundo projeto de React

---
+++ segundo/src/Footer.js
@@ -0,0 +1,16 @@
+function Footer() {
+    return (
+        <div className="footer">
+            <h3>
+                Consulte a programação<br />
+                completa em: 
+                <a href="http://www.sp.senac.br/tatuape">
+                    &nbsp;sp.senac.br/tatuape 
+                </a> <br />
+                Escaneie o QR Code
+            </h3> 
+        </div>
+    )
+}
+
+export default Footer;
---
+++ segundo/src/Main.js
@@ -0,0 +1,30 @@
+function Main() {
+    return (
+        <div className="main">
+            <h4>Bem Estar</h4>
+            <ul>
+                <li>Reiki-Master</li>
+                <li>Reiki-Nível 1 e 2</li>
+                <li>Reiki-Nível 3</li>
+            </ul>
+
+            <h4>Tecnologia da Informação</h4>
+            <ul>
+                <li>Autocad - Projetos em 2D</li>
+                <li>Excel 2019</li>
+                <li>Excel 2019 - Avançado</li>
+                <li>Introdução ao Office - Word, Excel e PowerPoint 2019</li>
+                <li>Photoshop - Tratamento de Imagem</li>
+                <li>Sketchup PRO</li>
+                <li>Wordpress | - Criação e Gerenciamento de Sites</li>
+            </ul>
+
+            <img
+             class="img" 
+             alt="business"
+             src="https://images.unsplash.com/photo-1638913662584-731da41f5a59?ixlib=rb-1.2.1ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8auto=formatfit=cropw=1170q=80"></img>
+        </div>
+    )
+}
+
+export default Main;
